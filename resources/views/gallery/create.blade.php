d3fce51246e2d0585e6eddd57d879e7fa4f62d08
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 17:48:51 +0530

    create gallery view temlate

---
+++ resources/views/gallery/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts/main')
+@section('content')
+
+    <div class="callout primary">
+      <article class="grid-container">
+        <div class="row column">
+          <h1>Create Gallery.</h1>
+        </div>
+      </article>
+    </div>
+    
+    <article class="grid-container">
+      <div class="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
+        <div style="padding:30px;">
+            create
+
+        </div>
+      </div>
+    </article>
+
+@endsection
