1084d4cd20b4a91adedd8c1efaeabefd71d635e9
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 17:49:06 +0530

    index gallery virew template

---
+++ resources/views/gallery/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts/main')
+@section('content')
+
+
+    <div class="callout primary">
+      <article class="grid-container">
+        <div class="">
+          <h1>Hello! This is the Gallery of a very witty person.</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna
+            ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra.
+            Nulla fringilla.</p>
+        </div>
+      </article>
+    </div>
+    
+<article class="grid-container">
+      <div class="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+        <div class="cell">
+          <img class="thumbnail" src="https://placehold.it/550x550">
+          <h5>My Site</h5>
+        </div>
+      </div>
+      <hr>
+    </article>    
+@endsection
