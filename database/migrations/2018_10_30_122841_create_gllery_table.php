e44081c617358a971dabb7935680e5b66f513e40
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-31 10:49:14 +0530

    create mgrations

--- database/migrations/2018_10_30_122841_create_gllery_table.php
+++ database/migrations/2018_10_30_122841_create_gllery_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGlleryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('galleries', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('owner_id');
             $table->string('name');
-            $table->string('email');
-          //  $table->timestamp('email_verified_at');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('cover_image');
+            
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('galleries');
     }
 }
--- database/migrations/2018_10_30_124238_create_photos_table.php
+++ database/migrations/2018_10_30_124238_create_photos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
+           // $table->increments('id');
             $table->increments('id');
+            $table->integer('owner_id');
             $table->string('name');
-            $table->string('email');
-          //  $table->timestamp('email_verified_at');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('image');
+           
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('photos');
     }
 }
