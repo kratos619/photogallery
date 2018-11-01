98be88b1780b048c8f22938ac60ce4a4a6ca1e48
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-01 09:10:29 +0530

    make page redirect

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/gallery';
 
     /**
      * Create a new controller instance.
